0a7a9eca10591119f44541177c5a07264dcba608
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-09-07 14:12:13 +0530

    Using https for interservice communication
    Signed-off-by: Kamesh Sampath <kamesh.sampath@hotmail.com>

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,11 +1,19 @@
+const fs = require("fs");
+const https = require("https");
 const axios = require("axios");
+
+const sslOpts = new https.Agent({
+  ca: fs.readFileSync(
+    "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
+  )
+});
 
 export async function getQuestions() {
   try {
     const url =
       process.env.VUE_APP_QUESTIONS_API_URL ||
       "http://myapi.example.com/api/questions";
-    const res = await axios.get(url);
+    const res = await axios.get(url, { httpsAgent: sslOpts });
     return res.data;
   } catch (error) {
     console.error(error);
@@ -17,7 +25,7 @@ export async function getFrames() {
     const url =
       process.env.VUE_APP_FRAMES_API_URL ||
       "http://myapi.example.com/api/frames";
-    const res = await axios.get(url);
+    const res = await axios.get(url, { httpsAgent: sslOpts });
     return res.data;
   } catch (error) {
     console.error(error);
@@ -29,7 +37,7 @@ export async function getFrame(id) {
     const url =
       `${process.env.VUE_APP_FRAMES_API_URL}/${id}` ||
       `http://myapi.example.com/api/frames/${id}`;
-    const res = await axios.get(url);
+    const res = await axios.get(url, { httpsAgent: sslOpts });
     return res.data;
   } catch (error) {
     console.error(error);
@@ -41,7 +49,10 @@ export async function saveUserResponse(fromData) {
     const res = await axios.post(
       `${process.env.VUE_APP_COLLABORATORS_API_URL}`,
       fromData,
-      { headers: { "Content-Type": "multipart/form-data" } }
+      {
+        httpsAgent: sslOpts,
+        headers: { "Content-Type": "multipart/form-data" }
+      }
     );
     return res;
   } catch (err) {
@@ -53,7 +64,8 @@ export async function saveUserResponse(fromData) {
 export async function getAvatars() {
   try {
     const res = await axios.get(
-      `${process.env.VUE_APP_COLLABORATORS_API_URL}/avatars`
+      `${process.env.VUE_APP_COLLABORATORS_API_URL}/avatars`,
+      { httpsAgent: sslOpts }
     );
     return res;
   } catch (err) {
