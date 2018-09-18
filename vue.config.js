31089acfce54bfa5dd55003d7088d589d5990e9c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-09-19 00:31:42 +0530

    Using nginx.conf copy via webpack

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   configureWebpack: {
     devServer: {
       historyApiFallback: true
-    }
+    },
+    plugins: [new CopyWebpackPlugin([{ from: "ngnix.conf", to: "ngnix.conf" }])]
   }
 };
