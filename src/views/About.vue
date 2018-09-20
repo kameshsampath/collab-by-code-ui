010395bdfceae624af91f3fd8f52d24e223e8916
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-09-20 09:44:30 +0530

    Added texts from Alexa

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div>
+      <q>
+        We wanted to create an open source project called “Collaborate by Code” that taught the principles of open source to the Red Hat community in an engaging and innovative way.
+      </q>
+      <p> -Grace C. and Fiona W.</p>
+    </div>
+    <p>
+      Collaborate by Code was created by two Red Hat interns, Fiona and Grace, during their 2018 summer internships in our Boston office. We loved what they started, so at the end of the intern program, a team of Red Hatters volunteered to continue developing the application to create what you see today. True to its name, Collaborate by Code has been a very collaborative project and a great open source story. Interested in reviewing or contributing to the code? The project is available on the Red Hat Official GitHub.
+    </p>
   </div>
 </template>
+<style scoped>
+</style>
+
