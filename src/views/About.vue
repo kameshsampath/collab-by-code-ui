6f7ad6f9b8ddcc6dff3741428667e6d7cadd98ec
jcaloger <jcaloger@redhat.com>
2018-09-20 22:35:43 -0400

    About page layout updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="container">
-    <div>
-      <q>
-        We wanted to create an open source project called “Collaborate by Code” that taught the principles of open source to the Red Hat community in an engaging and innovative way.
-      </q>
-      <p> -Grace C. and Fiona W.</p>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-8">
+        <h4 class="mt-4">Collaborate by Code was created by two Red Hat interns, Fiona and Grace, during their 2018 summer internships in our Boston office. </h4>
+        <p class="mt-4">
+        We loved what they started, so at the end of the intern program, a team of Red Hatters volunteered to continue developing the application to create what you see today. True to its name, Collaborate by Code has been a very collaborative project and a great open source story. Interested in reviewing or contributing to the code? The project is available on the Red Hat Official GitHub.
+        </p>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <p>We wanted to create an open source project called “Collaborate by Code” that taught the principles of open source to the Red Hat community in an engaging and innovative way.</p>
+              <footer class="blockquote-footer">Grace C. and Fiona W.</footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
     </div>
-    <p>
-      Collaborate by Code was created by two Red Hat interns, Fiona and Grace, during their 2018 summer internships in our Boston office. We loved what they started, so at the end of the intern program, a team of Red Hatters volunteered to continue developing the application to create what you see today. True to its name, Collaborate by Code has been a very collaborative project and a great open source story. Interested in reviewing or contributing to the code? The project is available on the Red Hat Official GitHub.
-    </p>
   </div>
 </template>
 <style scoped>
